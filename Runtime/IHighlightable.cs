740bafd676d1623ebed92580d7313d6d801a220a
Hyago Oliveira <hyagogow@gmail.com>
2023-06-13 11:24:52 -0300

    Add IHighlightable interface

---
+++ Runtime/IHighlightable.cs
@@ -0,0 +1,11 @@
+namespace ActionCode.VisualEffects
+{
+    /// <summary>
+    /// Interface used on objects able to be highlighted/unhighlighted.
+    /// </summary>
+    public interface IHighlightable
+    {
+        void Highlight();
+        void UnHighlight();
+    }
+}
